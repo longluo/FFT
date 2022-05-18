ed22f64e485646a9ab1e38c796daf2c9f8d07538
longluo <calmbody@163.com>
2022-05-18 15:52:46 +0800

    FFT Iteration v3 opt space

--- code/cpp/fft_iteration_multiply_v3.cpp
+++ code/cpp/fft_iteration_multiply_v3.cpp
@@ -156,7 +156,6 @@ public:
         }
 
         vector<Complex> a(n);
-        vector<Complex> b(n);
 
         for (int i = 0; i < len1 && i < len2; i++) {
             a[i] = Complex(num1[len1 - 1 - i] - '0', num2[len2 - 1 - i] - '0');
