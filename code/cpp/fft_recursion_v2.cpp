00ff060922b47561c33474a016dd4fe3334ead33
longluo <calmbody@163.com>
2022-05-18 12:47:00 +0800

    FFT Recursion v2 Test OK

--- code/cpp/fft_recursion_v2.cpp
+++ code/cpp/fft_recursion_v2.cpp
@@ -6,196 +6,153 @@
 
 using namespace std;
 
-const double PI = acos(-1.0);    // PI = arccos(-1)
+class Solution {
+public:
+    const double PI = acos(-1.0);  // PI = arccos(-1)
 
-// Complex Number
-struct Complex {
-    double x, y;
+    void display(vector<complex<double>> num) {
+        for (auto &cd : num) {
+            cout << cd;
+        }
 
-    Complex(double _x = 0.0, double _y = 0.0) {
-        x = _x;
-        y = _y;
+        cout << endl;
     }
 
-    Complex operator-(const Complex &b) const {
-        return Complex(x - b.x, y - b.y);
-    }
+    /**
+     * FFT Recursion 实现
+     *
+     * @param a
+     * @param invert true means IFFT, else FFT
+     * @return y
+     */
+    vector<complex<double>> FFT(vector<complex<double>> &a, bool invert) {
+        //第一个参数为一个多项式的系数, 以次数从小到大的顺序, 向量中每一项的实部为该项系数
+        int n = a.size();
 
-    Complex operator+(const Complex &b) const {
-        return Complex(x + b.x, y + b.y);
-    }
+        // 如果当前多项式仅有常数项时直接返回多项式的值
+        if (n == 1) {
+            return a;
+        }
 
-    Complex operator*(const Complex &b) const {
-        return Complex(x * b.x - y * b.y, x * b.y + y * b.x);
-    }
-};
+        vector<complex<double>> Pe(n / 2), Po(n / 2); // 文中的Pe与Po的系数表示法
 
-/**
- * FFT Recursion 实现
- *
- * @param a
- * @param invert true means IFFT, else FFT
- * @return y
- */
-vector<complex<double>> FFT(vector<complex<double>> &a, bool invert) {
-    //第一个参数为一个多项式的系数, 以次数从小到大的顺序, 向量中每一项的实部为该项系数
-    int n = a.size();
+        for (int i = 0; 2 * i < n; i++) {
+            Pe[i] = a[2 * i];
+            Po[i] = a[2 * i + 1];
+        }
 
-    // 如果当前多项式仅有常数项时直接返回多项式的值
-    if (n == 1) {
-        return a;
-    }
+        // Divide 分治
+        // 递归求 ye = Pe(xi), yo = Po(xi)
+        vector<complex<double>> ye = FFT(Pe, invert);
+        vector<complex<double>> yo = FFT(Po, invert);
 
-    vector<complex<double>> Pe(n / 2), Po(n / 2); // 文中的Pe与Po的系数表示法
+        // Combine
+        vector<complex<double>> y(n);
 
-    for (int i = 0; 2 * i < n; i++) {
-        Pe[i] = a[2 * i];
-        Po[i] = a[2 * i + 1];
-    }
+        // Root of Units
+        double ang = 2 * PI / n * (invert ? -1 : 1);
+        complex<double> wn(cos(ang), sin(ang)); // wn为第1个n次复根,
+        complex<double> w(1, 0);   // w为第零0个n次复根, 即为 1
 
-    // Divide 分治
-    // 递归求 ye = Pe(xi), yo = Po(xi)
-    vector<complex<double>> ye = FFT(Pe, invert);
-    vector<complex<double>> yo = FFT(Po, invert);
+        for (int i = 0; i < n / 2; i++) {
+            y[i] = ye[i] + w * yo[i];  // 求出P(xi)
+            y[i + n / 2] = ye[i] - w * yo[i]; // 由单位复根的性质可知第k个根与第k + n/2个根互为相反数
+            w *= wn;  // w * wn 得到下一个复根
+        }
 
-    // Combine
-    vector<complex<double>> y(n);
-
-    // Root of Units
-    double ang = 2 * PI / n * (invert ? -1 : 1);
-    complex<double> wn(cos(ang), sin(ang)); // wn为第1个n次复根,
-    complex<double> w(1, 0);   // w为第零0个n次复根, 即为 1
-
-    for (int i = 0; i < n / 2; i++) {
-        y[i] = ye[i] + w * yo[i];  // 求出P(xi)
-        y[i + n / 2] = ye[i] - w * yo[i]; // 由单位复根的性质可知第k个根与第k + n/2个根互为相反数
-        w *= wn;  // w * wn 得到下一个复根
-    }
-
-    // IFFT最终结果需要除以 n ，其中 n 为不小于原多项式阶+1的最小2的整数次幂
+        // IFFT最终结果需要除以 n ，其中 n 为不小于原多项式阶+1的最小2的整数次幂
 //    if (invert) {
 //        for (auto &cd : y) {
 //            cd /= n;
 //        }
 //    }
 
-    return y;  // 返回最终的系数
-}
-
-//获取系数, 注意需要从右向左获取(从0次幂的系数开始)
-vector<complex<double>> read() {
-    string num;
-    cin >> num;
-    vector<complex<double>> result;
-    for (int i = num.size() - 1; i >= 0; i--) {
-        complex<double> tmp(num[i] - '0', 0);
-        result.push_back(tmp);
+        return y;  // 返回最终的系数
     }
 
-    return result;
-}
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") {
+            return "0";
+        }
 
-// 多项式系数表达式的修饰
-void solve(vector<complex<double>> &a, vector<complex<double>> &b) {
-    complex<double> tmp(0, 0);
-    int sum = a.size() + b.size();
+        int len1 = num1.size();
+        int len2 = num2.size();
 
-    while (a.size() < sum) {
-        a.push_back(tmp);
+        int n = 1;
+        while (n < len1 + len2) {
+            n = n << 1;
+        }
+
+        vector<complex<double>> a(n);
+        vector<complex<double>> b(n);
+
+        for (int i = len1 - 1; i >= 0; i--) {
+            a[i] = complex<double>(num1[len1 - 1 - i] - '0', 0);
+        }
+
+        for (int i = len2 - 1; i >= 0; i--) {
+            b[i] = complex<double>(num2[len2 - 1 - i] - '0', 0);
+        }
+
+        cout << "Before FFT" << endl;
+        display(a);
+        display(b);
+
+        vector<complex<double>> midA, midB;
+
+        midA = FFT(a, false);
+        midB = FFT(b, false);
+
+        cout << "After FFT" << endl;
+        display(midA);
+        display(midB);
+
+        vector<complex<double>> mid(n);
+        for (int i = 0; i < n; i++) {
+            mid[i] = midA[i] * midB[i];
+        }
+
+        cout << "Times:" << endl;
+        display(mid);
+
+        vector<complex<double>> result;
+        result = FFT(mid, true);
+
+        cout << "Result:" << endl;
+        display(result);
+
+        string ans;
+        int carry = 0;
+        for (int i = 0; i < result.size(); i++) {
+            int sum = round(round(result[i].real()) / result.size()) + carry;
+            carry = sum / 10;
+            ans += sum % 10 + '0';
+        }
+
+        if (carry > 0) {
+            ans += carry % 10 + '0';
+        }
+
+        int idx = ans.size() - 1;
+        while (ans[idx] == '0' && idx > 0) {
+            idx--;
+        }
+
+        ans = ans.substr(0, idx + 1);
+        cout << "Output: " << ans << endl;
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
-
-    while (b.size() < sum) {
-        b.push_back(tmp);
-    }
-
-    //如果两式的阶不同, 先补齐
-    int temp = 1;
-    while (temp < a.size()) {
-        temp <<= 1;
-    }
-
-    //获取不小于n的最小2的整数次幂
-    while (a.size() < temp) {
-        a.push_back(tmp);
-        b.push_back(tmp);
-    }
-
-    //补齐
-}
-
-void display(vector<complex<double>> num) {
-    for (auto &cd : num) {
-        cout << cd;
-    }
-
-    cout << endl;
-}
+};
 
 int main() {
-    vector<complex<double>> num1, num2;
+    cout << "Please Input 2 Numbers:" << endl;
+    string num1, num2;
+    cin >> num1 >> num2;
 
-    num1 = read();
-    num2 = read();
+    Solution s;
+    string result = s.multiply(num1, num2);
 
-    vector<complex<double>> tmp1, tmp2, mid, ans;
-    solve(num1, num2);
-
-    cout << "Input: " << endl;
-    cout << "num1 = ";
-    display(num1);
-    cout << "num2 = ";
-    display(num2);
-
-    tmp1 = FFT(num1, false);
-    tmp2 = FFT(num2, false);
-
-    num1.clear();
-    num2.clear();
-
-    for (int i = 0; i < tmp1.size(); i++) {
-        mid.push_back(tmp1[i] * tmp2[i]);
-    }
-
-    cout << "mid = ";
-    display(mid);
-
-    tmp1.clear();
-    tmp2.clear();
-
-    ans = FFT(mid, true);
-    bool Ans = false;
-    int add = 0;
-    string final;
-
-    cout << "Output: ";
-    display(ans);
-
-    //进位处理
-    for (int i = 0; i < ans.size(); i++) {
-        int op = round(round(ans[i].real()) / ans.size()) + add;
-        add = 0;
-        if (op >= 10) {
-            add = op / 10;
-        }
-
-        final += op % 10 + '0';
-    }
-
-    if (add > 0) {
-        final += add % 10 + '0';
-    }
-
-    cout << "Result is: " << endl;
-
-    for (int i = final.size() - 1; i >= 0; i--) {
-        if (final[i] != '0') {
-            Ans = true;
-        } else if (!Ans) {
-            continue;
-        }
-
-        cout << final[i];
-    }
-
-    cout << '\n';
+    cout << "Result is:" << endl;
+    cout << num1 << " x " << num2 << "=" << result << endl;
 }
