7f138cbaaa52ff97d5d6d962a5e86c8eda672d2b
longluo <calmbody@163.com>
2022-05-19 15:42:59 +0800

    NTT v5 Recursion

---
+++ code/cpp/ntt_recursion_multiply.cpp
@@ -0,0 +1,148 @@
+//
+// Created by longluo on 2022/5/18.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void display(vector<long long> arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+
+class Solution {
+
+public:
+    const long long G = 3;
+    const long long G_INV = 332748118;
+    const long long MOD = 998244353;
+
+    vector<int> rev;
+
+    long long quickPower(long long a, long long b) {
+        long long res = 1;
+
+        while (b > 0) {
+            if (b & 1) {
+                res = (res * a) % MOD;
+            }
+
+            a = (a * a) % MOD;
+            b >>= 1;
+        }
+
+        return res % MOD;
+    }
+
+    void ntt(vector<long long> &a, bool invert) {
+        int n = a.size();
+
+        if (n == 1) {
+            return;
+        }
+
+        vector<long long> Pe(n / 2), Po(n / 2);
+
+        for (int i = 0; 2 * i < n; i++) {
+            Pe[i] = a[2 * i];
+            Po[i] = a[2 * i + 1];
+        }
+
+        ntt(Pe, invert);
+        ntt(Po, invert);
+
+        long long wn = quickPower(invert ? G_INV : G, (MOD - 1) / n);
+        long long w = 1;
+
+        for (int i = 0; i < n / 2; i++) {
+            a[i] = Pe[i] + w * Po[i] % MOD;
+            a[i] = (a[i] % MOD + MOD) % MOD;
+            a[i + n / 2] = Pe[i] - w * Po[i] % MOD;
+            a[i + n / 2] = (a[i + n / 2] % MOD + MOD) % MOD;
+            w = w * wn % MOD;
+        }
+    }
+
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") {
+            return "0";
+        }
+
+        int len1 = num1.size();
+        int len2 = num2.size();
+
+        int n = 1;
+
+        while (n < (len1 + len2)) {
+            n = n << 1;
+        }
+
+        vector<long long> a(n, 0), b(n, 0);
+
+        for (int i = 0; i < len1; ++i) {
+            a[i] = num1[len1 - 1 - i] - '0';
+        }
+
+        for (int i = 0; i < len2; ++i) {
+            b[i] = num2[len2 - 1 - i] - '0';
+        }
+
+        ntt(a, false);
+        ntt(b, false);
+
+        for (int i = 0; i < n; i++) {
+            a[i] = (a[i] * b[i]) % MOD;
+        }
+
+        ntt(a, true);
+
+        string res;
+        long long carry = 0;
+        long long inver = quickPower(n, MOD - 2);
+
+        for (int i = 0; i < n; i++) {
+            a[i] = a[i] * inver % MOD;
+        }
+
+        for (int i = 0; i < n; i++) {
+            long long sum = a[i] + carry;
+            res += sum % 10 + '0';
+            carry = sum / 10;
+        }
+
+        while (carry) {
+            res += carry % 10 + '0';
+            carry /= 10;
+        }
+
+        int idx = n - 1;
+        while (idx >= 0 && res[idx] == '0') {
+            idx--;
+        }
+
+        res = res.substr(0, idx + 1);
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+int main() {
+    cout << "Please Input 2 numbers: " << endl;
+
+    string num1, num2;
+    cin >> num1 >> num2;
+
+    Solution s;
+    string result = s.multiply(num1, num2);
+
+    cout << "Result:" << result << endl;
+}
+
+
