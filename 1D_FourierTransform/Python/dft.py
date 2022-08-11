4bfc0a0cc6c7d466724fc009dafce8c0b5397312
longluo <calmbody@163.com>
2022-08-11 22:04:54 +0800

    dft.py

---
+++ 1D_FourierTransform/Python/dft.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('seaborn-poster')
+
+# sampling rate
+sr = 100
+# sampling interval
+ts = 1.0/sr
+t = np.arange(0,1,ts)
+
+freq = 1.
+x = 3*np.sin(2*np.pi*freq*t)
+
+freq = 4
+x += np.sin(2*np.pi*freq*t)
+
+freq = 7
+x += 0.5* np.sin(2*np.pi*freq*t)
+
+plt.figure(figsize = (8, 6))
+plt.plot(t, x, 'r')
+plt.ylabel('Amplitude')
+
+plt.show()
+
+
+
+
+
+
+
