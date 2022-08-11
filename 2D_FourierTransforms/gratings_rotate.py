c8cfafcb342010b5894baf7f596c3e1c30ef3284
longluo <calmbody@163.com>
2022-08-11 22:40:52 +0800

    gratings_rotate.py

---
+++ 2D_FourierTransforms/gratings_rotate.py
@@ -0,0 +1,19 @@
+# gratings.py
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-500, 501, 1)
+
+X, Y = np.meshgrid(x, x)
+
+wavelength = 200
+angle = np.pi / 9
+grating = np.sin(
+    2*np.pi*(X*np.cos(angle) + Y*np.sin(angle)) / wavelength
+)
+
+plt.set_cmap("gray")
+plt.imshow(grating)
+plt.show()
+
