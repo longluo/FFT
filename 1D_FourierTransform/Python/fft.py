3695646e2524ad876fe6a952a4a0ffa716b12ab6
longluo <calmbody@163.com>
2022-08-11 22:17:19 +0800

    fft measure time

--- 1D_FourierTransform/Python/fft.py
+++ 1D_FourierTransform/Python/fft.py
@@ -1,8 +1,12 @@
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 plt.style.use('seaborn-poster')
 
+
+# %matplotlib inline
 
 def FFT(x):
     """
@@ -26,37 +30,37 @@ def FFT(x):
              X_even + factor[int(N / 2):] * X_odd])
         return X
 
+
 # sampling rate
 sr = 128
 # sampling interval
-ts = 1.0/sr
-t = np.arange(0,1,ts)
+ts = 1.0 / sr
+t = np.arange(0, 1, ts)
 
 freq = 1.
-x = 3*np.sin(2*np.pi*freq*t)
+x = 3 * np.sin(2 * np.pi * freq * t)
 
 freq = 4
-x += np.sin(2*np.pi*freq*t)
+x += np.sin(2 * np.pi * freq * t)
 
 freq = 7
-x += 0.5* np.sin(2*np.pi*freq*t)
+x += 0.5 * np.sin(2 * np.pi * freq * t)
 
-plt.figure(figsize = (8, 6))
+plt.figure(figsize=(8, 6))
 plt.plot(t, x, 'r')
 plt.ylabel('Amplitude')
 
 plt.show()
 
-
-X=FFT(x)
+X = FFT(x)
 
 # calculate the frequency
 N = len(X)
 n = np.arange(N)
-T = N/sr
-freq = n/T
+T = N / sr
+freq = n / T
 
-plt.figure(figsize = (12, 6))
+plt.figure(figsize=(12, 6))
 plt.subplot(121)
 plt.stem(freq, abs(X), 'b', \
          markerfmt=" ", basefmt="-b")
@@ -64,12 +68,12 @@ plt.xlabel('Freq (Hz)')
 plt.ylabel('FFT Amplitude |X(freq)|')
 
 # Get the one-sided specturm
-n_oneside = N//2
+n_oneside = N // 2
 # get the one side frequency
 f_oneside = freq[:n_oneside]
 
 # normalize the amplitude
-X_oneside =X[:n_oneside]/n_oneside
+X_oneside = X[:n_oneside] / n_oneside
 
 plt.subplot(122)
 plt.stem(f_oneside, abs(X_oneside), 'b', \
@@ -79,5 +83,26 @@ plt.ylabel('Normalized FFT Amplitude |X(freq)|')
 plt.tight_layout()
 plt.show()
 
+
+def gen_sig(sr):
+    '''
+    function to generate
+    a simple 1D signal with
+    different sampling rate
+    '''
+    ts = 1.0 / sr
+    t = np.arange(0, 1, ts)
+
+    freq = 1.
+    x = 3 * np.sin(2 * np.pi * freq * t)
+    return x
+
+# sampling rate =2048
+sr = 2048
+
+tis1 = time.perf_counter()
+FFT(gen_sig(sr))
+tis2 = time.perf_counter()
+print(tis2 - tis1)
 
 
