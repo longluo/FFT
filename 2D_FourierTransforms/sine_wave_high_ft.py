7b4a2f5cdb382dafa247437ca5afa2cd9ff651ce
longluo <calmbody@163.com>
2022-08-11 22:42:32 +0800

    sine_wave_high_ft.py

---
+++ 2D_FourierTransforms/sine_wave_high_ft.py
@@ -0,0 +1,31 @@
+# gratings.py
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-500, 501, 1)
+
+X, Y = np.meshgrid(x, x)
+
+wavelength = 100
+angle = 0
+grating = np.sin(
+    2*np.pi*(X*np.cos(angle) + Y*np.sin(angle)) / wavelength
+)
+
+plt.set_cmap("gray")
+
+plt.subplot(121)
+plt.imshow(grating)
+
+# Calculate Fourier transform of grating
+ft = np.fft.ifftshift(grating)
+ft = np.fft.fft2(ft)
+ft = np.fft.fftshift(ft)
+
+plt.subplot(122)
+plt.imshow(abs(ft))
+plt.xlim([480, 520])
+plt.ylim([520, 480])  # Note, order is reversed for y
+plt.show()
+
