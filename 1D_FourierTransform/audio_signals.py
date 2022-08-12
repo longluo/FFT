26ab21eae1618d391e08d32be29d040638b52a90
longluo <calmbody@163.com>
2022-08-12 10:43:48 +0800

    440 music

--- 1D_FourierTransform/audio_signals.py
+++ 1D_FourierTransform/audio_signals.py
@@ -22,6 +22,23 @@ plt.xlabel('Time (x)')
 plt.ylabel('Amplitude (y)')
 plt.show()
 
+
+# Pure Music Signal
+_, music_tone = generate_sine_wave(440, SAMPLE_RATE, DURATION)
+
+normalized_tone = np.int16((music_tone / music_tone.max()) * 32767)
+
+plt.plot(normalized_tone[:1000])
+plt.xlabel('Time (x)')
+plt.ylabel('Amplitude (y)')
+plt.title('Music 440Hz')
+plt.show()
+
+# Remember SAMPLE_RATE = 44100 Hz is our playback rate
+write("440music.wav", SAMPLE_RATE, normalized_tone)
+
+
+
 # Mixing Audio Signals
 _, nice_tone = generate_sine_wave(440, SAMPLE_RATE, DURATION)
 _, noise_tone = generate_sine_wave(4000, SAMPLE_RATE, DURATION)
@@ -32,6 +49,8 @@ mixed_tone = nice_tone + noise_tone
 normalized_tone = np.int16((mixed_tone / mixed_tone.max()) * 32767)
 
 plt.plot(normalized_tone[:1000])
+plt.xlabel('Time (x)')
+plt.ylabel('Amplitude (y)')
 plt.show()
 
 # Remember SAMPLE_RATE = 44100 Hz is our playback rate
@@ -45,4 +64,6 @@ yf = fft(normalized_tone)
 xf = fftfreq(N, 1 / SAMPLE_RATE)
 
 plt.plot(xf, np.abs(yf))
+plt.xlabel('freq (w)')
+plt.ylabel('Amplitude (y)')
 plt.show()
